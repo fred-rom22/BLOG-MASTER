2614e18bfa33bacbddc636feaa90c7f48da90a97
fred-rom22 <38973745+fred-rom22@users.noreply.github.com>
2020-04-30 23:06:38 +0000

    Create dev.php

---
+++ config/dev.php
@@ -0,0 +1,9 @@
+<?php
+
+const HOST = 'localhost';
+const DB_NAME = 'blog';
+const CHARSET = 'utf8';
+const DB_HOST = 'mysql:host='.HOST.';dbname='.DB_NAME.';charset='.CHARSET;
+const DB_USER = 'root';
+const DB_PASS = '';
+?>
